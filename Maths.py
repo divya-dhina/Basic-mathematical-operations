72db8633cb0028a17d6113d6ac407450c418da2b
divya-dhina <121435130+divya-dhina@users.noreply.github.com>
2022-12-26 18:13:13 +0530

    Create Maths.py

---
+++ Maths.py
@@ -0,0 +1,31 @@
+def add(x,y):
+  z=x+y
+  return z
+def sub(x,y):
+  z=x-y
+  return z
+def mul(x,y):
+  z=x*y
+  return z
+def div(x,y):
+  z=x/y
+  return z
+def expo(x,y):
+  z=x**y
+  return z
+def modu(x,y):
+  z=x%y
+  return z
+def fd(x,y):
+  z=x//
+  return z
+  
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+print(expo(a,b))
+print(modu(a,b))
+print(fd(a,b))
